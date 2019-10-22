c94c0d79930cbbee881d698b490d3ba87a82bf91
Hasan Khan <hassankhankabacha@gmail.com>
2019-10-22 17:29:16 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,4 +1,16 @@
-Class Post 
-attr_accessor :title, :author 
+class Post
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
 end
 
